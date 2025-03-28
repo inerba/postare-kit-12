b2865790aa8645e8a965e8cf3e533207f95fc6b8
Francesco <inerba@gmail.com>
2025-03-28 14:27:45 +0100

    Aggiunta della gestione dei menu: creazione del modello 'Menu', risorse e pagine per la creazione, modifica e visualizzazione dei menu. Inclusi file di traduzione e migrazione per la tabella 'menus'. Aggiornato composer.json per includere la dipendenza 'saade/filament-adjacency-list'.

---
+++ lang/en/simple-menu-manager.php
@@ -0,0 +1,27 @@
+<?php
+
+// filepath: d:\local\www\postare-kit-12\lang\en\simple-menu-manager.php
+return [
+    'resource' => [
+        'name' => 'Name',
+        'slug' => 'Slug',
+        'items' => 'Menu Items',
+        'add_item' => 'Add Item',
+        'type' => 'Type',
+        'label' => 'Label',
+    ],
+    'handlers' => [
+        'link' => [
+            'name' => 'Link',
+        ],
+        'page' => [
+            'name' => 'Page',
+        ],
+        'placeholder' => [
+            'name' => 'Placeholder',
+        ],
+    ],
+    'common' => [
+        'advanced_settings' => 'Advanced Settings',
+    ],
+];
