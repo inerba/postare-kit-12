eb8ed7fbab281f1318b4d07a27f13b5581502385
Francesco <inerba@gmail.com>
2025-04-02 17:54:41 +0200

    Riorganizzazione degli import e aggiunta di commenti per la gestione delle rotte statiche

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('home');
 });
 
+/**
+ * Rotte per le pagine statiche
+ * Se non utilizzi un prefisso, assicurati che queste rotte siano sempre dichiarate alla fine
+ * per evitare conflitti con altre rotte già definite.
+ */
 Route::prefix(config('postare-kit.pages_prefix'))
     ->middleware(config('postare-kit.middleware', 'web')) // Altrimenti non funzionano i binding
     ->name('cms.')
