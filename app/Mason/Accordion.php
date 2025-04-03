2b673923903162a26394a05ff7a661a62d8106bd
Francesco <inerba@gmail.com>
2025-04-03 12:29:37 +0200

    Aggiunta del componente di condivisione sociale e aggiornamento delle etichette per Accordion e Reviews

--- app/Mason/Accordion.php
+++ app/Mason/Accordion.php
@@ -17,7 +17,7 @@ class Accordion
         return Brick::make('accordion')
             ->label('Accordion')
             ->modalHeading('Impostazioni accordion')
-            ->icon(new HtmlString('<svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="currentColor" viewBox="0 0 256 256"><path d="M216,80H184V48a16,16,0,0,0-16-16H40A16,16,0,0,0,24,48V176a8,8,0,0,0,13,6.22L72,154V184a16,16,0,0,0,16,16h93.59L219,230.22a8,8,0,0,0,5,1.78,8,8,0,0,0,8-8V96A16,16,0,0,0,216,80ZM66.55,137.78,40,159.25V48H168v88H71.58A8,8,0,0,0,66.55,137.78ZM216,207.25l-26.55-21.47a8,8,0,0,0-5-1.78H88V152h80a16,16,0,0,0,16-16V96h32Z"></path></svg>'))
+            ->icon(new HtmlString('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="size-5"><title>frequently-asked-questions</title><path d="M18,15H6L2,19V3A1,1 0 0,1 3,2H18A1,1 0 0,1 19,3V14A1,1 0 0,1 18,15M23,9V23L19,19H8A1,1 0 0,1 7,18V17H21V8H22A1,1 0 0,1 23,9M8.19,4C7.32,4 6.62,4.2 6.08,4.59C5.56,5 5.3,5.57 5.31,6.36L5.32,6.39H7.25C7.26,6.09 7.35,5.86 7.53,5.7C7.71,5.55 7.93,5.47 8.19,5.47C8.5,5.47 8.76,5.57 8.94,5.75C9.12,5.94 9.2,6.2 9.2,6.5C9.2,6.82 9.13,7.09 8.97,7.32C8.83,7.55 8.62,7.75 8.36,7.91C7.85,8.25 7.5,8.55 7.31,8.82C7.11,9.08 7,9.5 7,10H9C9,9.69 9.04,9.44 9.13,9.26C9.22,9.08 9.39,8.9 9.64,8.74C10.09,8.5 10.46,8.21 10.75,7.81C11.04,7.41 11.19,7 11.19,6.5C11.19,5.74 10.92,5.13 10.38,4.68C9.85,4.23 9.12,4 8.19,4M7,11V13H9V11H7M13,13H15V11H13V13M13,4V10H15V4H13Z" /></svg>'))
             ->slideOver()
             ->fillForm(fn(array $arguments): array => array_merge(
                 Macro\Theme::getArguments($arguments),
--- app/Mason/Reviews.php
+++ app/Mason/Reviews.php
@@ -19,11 +19,11 @@ class Reviews
     public static function make(): Brick
     {
         return Brick::make('reviews')
-            ->label('Reviews')
-            ->modalHeading('Reviews Settings')
+            ->label('Recensioni')
+            ->modalHeading('Impostazioni Recensioni')
             ->icon(new HtmlString('<svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="currentColor" viewBox="0 0 256 256"><path d="M216,80H184V48a16,16,0,0,0-16-16H40A16,16,0,0,0,24,48V176a8,8,0,0,0,13,6.22L72,154V184a16,16,0,0,0,16,16h93.59L219,230.22a8,8,0,0,0,5,1.78,8,8,0,0,0,8-8V96A16,16,0,0,0,216,80ZM66.55,137.78,40,159.25V48H168v88H71.58A8,8,0,0,0,66.55,137.78ZM216,207.25l-26.55-21.47a8,8,0,0,0-5-1.78H88V152h80a16,16,0,0,0,16-16V96h32Z"></path></svg>'))
             ->slideOver()
-            ->fillForm(fn (array $arguments): array => array_merge(
+            ->fillForm(fn(array $arguments): array => array_merge(
                 Macro\Theme::getArguments($arguments),
                 Macro\SectionHeader::getArguments($arguments),
                 Macro\ButtonsRepeater::getArguments($arguments),
@@ -88,19 +88,19 @@ class Reviews
                         Toggle::make('pagination')
                             ->label('Paginazione')
                             ->helperText('Mostra i puntini di navigazione in basso')
-                            ->visible(fn (Get $get) => $get('layout') === 'carousel')
+                            ->visible(fn(Get $get) => $get('layout') === 'carousel')
                             ->default(true),
 
                         Toggle::make('arrows')
                             ->label('Frecce')
                             ->helperText('Mostra le frecce di navigazione ai lati')
-                            ->visible(fn (Get $get) => $get('layout') === 'carousel')
+                            ->visible(fn(Get $get) => $get('layout') === 'carousel')
                             ->default(true),
 
                         Toggle::make('rewind')
                             ->label('Riavvolgi')
                             ->helperText("Riavvolgi all'inizio dopo l'ultimo elemento")
-                            ->visible(fn (Get $get) => $get('layout') === 'carousel')
+                            ->visible(fn(Get $get) => $get('layout') === 'carousel')
                             ->default(true),
                     ]),
                 Macro\ButtonsRepeater::getFields(),
