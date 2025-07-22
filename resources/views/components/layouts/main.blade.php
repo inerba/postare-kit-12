0e87be2ed37b95585227099d39bf1d096dbb4afa
Francesco <inerba@gmail.com>
2025-07-22 12:14:08 +0200

    feat(SEO): aggiunta la sezione SEO nel layout principale

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -3,30 +3,33 @@
 ])
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        {{-- SEO --}}
-        {{ $seo ?? '' }}
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        @livewireStyles
+    {{-- SEO --}}
+    {{ $seo ?? '' }}
 
-        <!-- Scripts and Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 
-        <!-- Additional Styles -->
-        @stack('styles')
-        @stack('header_scripts')
-    </head>
+    <!-- Scripts and Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    <body class="flex min-h-screen flex-col font-sans antialiased">
-        <x-partials.header />
-        <main class="flex-1">
-            {!! $slot !!}
-        </main>
-        <x-partials.footer />
-        @livewireScripts
-        @stack('scripts')
-    </body>
+    <!-- Additional Styles -->
+    @stack('styles')
+    @stack('header_scripts')
+</head>
+
+<body class="flex min-h-screen flex-col font-sans antialiased">
+    <x-partials.header />
+    <main class="flex-1">
+        {!! $slot !!}
+    </main>
+    <x-partials.footer />
+    @livewireScripts
+    @stack('scripts')
+</body>
+
 </html>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-layouts.main>
+    <x-slot:seo>
+        <x-seo :title="db_config('homepage.meta.seo.tag_title')" :description="db_config('homepage.meta.seo.meta_description')" :og_title="db_config('homepage.social.og.title')" :og_description="db_config('homepage.social.og.description')" :image="db_config('homepage.social.og_image')" />
+    </x-slot>
     <livewire:slider :slides="db_config('homepage.slides')" :settings="db_config('homepage.slider_settings')" />
     <x-page :content="db_config('homepage.content')" />
 </x-layouts.main>
+
