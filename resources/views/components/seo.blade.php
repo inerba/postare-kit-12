51cf288a1bc209dc89de6cfd2903b93419553fc9
Francesco <inerba@gmail.com>
2025-07-29 11:37:53 +0200

    feat(seo): aggiornati i metadati Open Graph e aggiunta gestione dei tempi di pubblicazione e modifica

--- resources/views/components/seo.blade.php
+++ resources/views/components/seo.blade.php
@@ -5,10 +5,10 @@
     'og_description' => '',
     'image' => '',
     'url' => '',
-    'type' => config('postare-kit.og.type', 'article'),
+    'type' => 'article',
     'published_time' => null,
     'modified_time' => null,
-    'fb_app_id' => config('postare-kit.og.fb_app_id', null),
+    'fb_app_id' => config('ams.facebbook_app_id', '169565392540441'),
 ])
 
 @php
@@ -24,23 +24,25 @@
 
 {{-- <meta name="generator" content="PostareKit" /> --}}
 
-{{-- indica ai motori di ricerca (come Google) che possono mostrare anteprime di immagini di grandi dimensioni nei risultati di ricerca. --}}
-<meta name="robots" content="max-image-preview:large" />
-
 {{-- Open Graph --}}
 <meta property="og:site_name" content="{{ config('app.name') }}" />
 <meta property="og:locale" content="{{ app()->getLocale() }}" />
 <meta property="og:title" content="{{ $og_title }}" />
 <meta property="og:description" content="{{ $og_description }}" />
 @if ($image)
+    {{-- indica ai motori di ricerca (come Google) che possono mostrare anteprime di immagini di grandi dimensioni nei risultati di ricerca. --}}
+    <meta name="robots" content="max-image-preview:large" />
     <meta property="og:image" content="{{ $image }}" />
+    <meta property="og:image:secure_url" content="{{ $image }}" />
 @endif
 
 <meta property="og:url" content="{{ $url }}" />
 <meta property="og:type" content="{{ $type }}" />
 @if ($published_time && $modified_time)
-    <meta property="article:published_time" content="{{ $published_time->toIso8601String() }}" />
-    <meta property="article:modified_time" content="{{ $modified_time->toIso8601String() }}" />
+    <meta property="article:published_time"
+        content="{{ $published_time instanceof \Carbon\Carbon ? $published_time->toIso8601String() : \Carbon\Carbon::parse($published_time)->toIso8601String() }}" />
+    <meta property="article:modified_time"
+        content="{{ $modified_time instanceof \Carbon\Carbon ? $modified_time->toIso8601String() : \Carbon\Carbon::parse($modified_time)->toIso8601String() }}" />
 @endif
 
 <meta property="fb:app_id" content="{{ $fb_app_id }}" />
--- resources/views/pages/page.blade.php
+++ resources/views/pages/page.blade.php
@@ -7,6 +7,40 @@
 @endphp
 
 <x-layouts.main>
+    <x-slot:seo>
+        @php
+            // Se non c'è un og_image, usa l'immagine principale
+            $og_image = $page->getFirstMedia('og_image');
+
+            $title = $page->meta['seo']['tag_title'] ?? $page->title;
+            $description = $page->meta['seo']['meta_description'] ?? mason_excerpt($page->content, 155);
+        @endphp
+        <x-seo>
+            <x-slot:title>
+                {{ $title }}
+            </x-slot>
+            <x-slot:description>
+                {{ $description }}
+            </x-slot>
+            <x-slot:image>
+                {{-- Se non c'è un og_image, usa l'immagine principale --}}
+                {{ $og_image ? $og_image->getUrl() : ($image ? $image->getUrl() : null) }}
+            </x-slot>
+
+            <x-slot:og_title>
+                {{ $page->meta['og']['title'] ?? $title }}
+            </x-slot>
+            <x-slot:og_description>
+                {{ $page->meta['og']['description'] ?? $description }}
+            </x-slot>
+
+            <x-slot:url>{{ request()->url() }}</x-slot>
+            <x-slot:type>article</x-slot>
+
+            <x-slot:published_time>{{ Carbon::parse($page->created_at) }}</x-slot>
+            <x-slot:modified_time>{{ $page->updated_at }}</x-slot>
+        </x-seo>
+    </x-slot>
     <div class="post-content" data-aos="fade-up">
         <!-- ======= Single Post Content ======= -->
         <div class="prose max-w-none">
@@ -17,10 +51,8 @@
                 <h1 class="mb-5 text-center leading-normal">{{ $page->title }}</h1>
             </div>
             @if ($image && $page->extras['show_featured_image'])
-                <div
-                    class="main-column aspect-[16/6] bg-cover bg-center bg-no-repeat"
-                    style="background-image: url('{{ $image->getUrl() }}')"
-                ></div>
+                <div class="main-column aspect-[16/6] bg-cover bg-center bg-no-repeat"
+                    style="background-image: url('{{ $image->getUrl() }}')"></div>
             @endif
 
             <x-page :content="$page->content" :data="$page" />
