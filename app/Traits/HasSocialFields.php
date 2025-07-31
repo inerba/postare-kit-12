b0cf6742d4c2f10008740a729b94f51f5628743e
Francesco <inerba@gmail.com>
2025-07-31 15:35:52 +0200

    fix(HasSocialFields): aggiornati i parametri del metodo getSocialFields per supportare la gestione della directory delle immagini

--- app/Traits/HasSocialFields.php
+++ app/Traits/HasSocialFields.php
@@ -10,9 +10,11 @@ trait HasSocialFields
      * Restituisce i campi social per una resource specifica.
      *
      * @param  string  $prefix  Il prefisso per i campi social.
+     * @param  bool  $useMediaLibrary  Indica se utilizzare la libreria media di Spatie o il normale file upload.
+     * @param  string  $directory  La directory in cui caricare le immagini se non si utilizza la libreria media.
      * @return array<int, Forms\Components\Component> Un array di componenti Filament.
      */
-    protected static function getSocialFields($prefix): array
+    protected static function getSocialFields($prefix, bool $useMediaLibrary = true, $directory = 'og-images'): array
     {
         return [
             Forms\Components\Section::make([
@@ -29,21 +31,38 @@ trait HasSocialFields
                     ->helperText(__('pages.social.description_helper'))
                     ->columnSpanFull(),
             ]),
-            Forms\Components\Section::make([
-                Forms\Components\SpatieMediaLibraryFileUpload::make('og_image')
-                    ->label(__('pages.social.image'))
-                    ->helperText(__('pages.social.image_helper'))
-                    ->imageEditor()
-                    ->imageEditorAspectRatios([
-                        null,
-                        '16:9',
-                        '4:3',
-                        '1:1',
-                    ])
-                    ->openable()
-                    ->image()
-                    ->collection('og_image'),
-            ]),
+            Forms\Components\Section::make(
+                $useMediaLibrary ? [
+                    // usa upload normale se true, altrimenti usa il file upload di Spatie
+                    Forms\Components\SpatieMediaLibraryFileUpload::make('og_image')
+                        ->label(__('pages.social.image'))
+                        ->helperText(__('pages.social.image_helper'))
+                        ->imageEditor()
+                        ->imageEditorAspectRatios([
+                            null,
+                            '16:9',
+                            '4:3',
+                            '1:1',
+                        ])
+                        ->openable()
+                        ->image()
+                        ->collection('og_image'),
+                ] : [
+                    Forms\Components\FileUpload::make($prefix.'.og.image')
+                        ->label(__('pages.social.image'))
+                        ->helperText(__('pages.social.image_helper'))
+                        ->directory($directory)
+                        ->imageEditor()
+                        ->imageEditorAspectRatios([
+                            null,
+                            '16:9',
+                            '4:3',
+                            '1:1',
+                        ])
+                        ->openable()
+                        ->image()
+                        ->columnSpanFull(),
+                ]),
         ];
     }
 }
